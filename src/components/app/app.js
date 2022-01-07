d87a8f4809a71ae5120e92d143d87c327a2d74d7
Aleksandrgit config --global user.email  aleksrowing232@gmail.com <aleksrowing232@gmail.com>
2022-01-07 19:59:54 +0300

    y

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,32 @@
-import React, {Component} from "react";
+import React from "react";
 import Header from "../header";
 import MainBg from "../mainbg";
 import AboutUs from "../aboutus";
 import Ourbest from "../ourbest";
 import Footer from "../footer";
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './app.css';
 
 
-export default class App extends Component{
-    render(){
+ const App=()=>{
+    
         return(
+             <Router>
                 <div>
-                    <Header/>
+                    <Header/> 
                     <MainBg/>
                     <AboutUs/>
                     <Ourbest/>
                     <Footer/>
+                    <Routes>
+                            <Route>
+                                
+                            </Route>
+                    </Routes>
                 </div>
+            </Router>
             )    
-    }
+    
 }
+
+export default App;
